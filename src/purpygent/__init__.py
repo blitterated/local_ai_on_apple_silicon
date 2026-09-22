7c22bd5e2f464a128e098fd6311c70f7599dabaf
blitterated <blitterated@proton.me>
2026-09-22 18:19:21 -0500

    Message role definitions comment

--- src/purpygent/__init__.py
+++ src/purpygent/__init__.py
@@ -10,6 +10,11 @@ def step1() -> None:
 
     response = client.chat.completions.create(
         model="Llama-3.2-3B-Instruct-8bit",
+
+        # Message roles:
+        #   user:      a prompt/query from a human user.
+        #   assistant: a response from the LLM.
+        #   system:    something that should be loaded and understood by the LLM at start of session.
         messages=[
             {"role": "user", "content": "Explain what an AI agent is in one sentence."},
         ],
