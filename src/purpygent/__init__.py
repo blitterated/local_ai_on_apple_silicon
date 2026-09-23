8d475316ca21071687f8ad5a6754df5b3cd22186
blitterated <blitterated@proton.me>
2026-09-22 22:27:02 -0500

    Fetch response from model running in oMLX using API key from gopass

--- src/purpygent/__init__.py
+++ src/purpygent/__init__.py
@@ -1,12 +1,34 @@
+import subprocess
 from openai import OpenAI
 
 
 def main() -> None:
-    step1()
+    key_path = "API/oMLX/api_key"
+    api_key = get_api_key(key_path)
+    step1(api_key)
 
 
-def step1() -> None:
-    client = OpenAI()
+def get_api_key(key_path) -> String:
+    try:
+        result = subprocess.run(
+            ["gopass", "show", "-o", key_path],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+
+        api_key = result.stdout.strip()
+        return api_key
+
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"gopass failed for '{key_path}': {e.stderr.strip()}")
+
+
+def step1(api_key) -> None:
+    client = OpenAI(
+        base_url="http://127.0.0.1:8000/v1",
+        api_key=api_key
+    )
 
     response = client.chat.completions.create(
         model="Llama-3.2-3B-Instruct-8bit",
